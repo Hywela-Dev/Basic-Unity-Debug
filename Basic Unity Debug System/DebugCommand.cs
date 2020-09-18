f9d3771c5a94db7b5de728f485819e2f83007065
Hywela-Dev <71149848+Hywela-Dev@users.noreply.github.com>
2020-09-18 11:16:12 +0100

    Add files via upload

---
+++ Basic Unity Debug System/DebugCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DebugCommandBase
+{
+    string _commandId;
+    string _commandDescription;
+    string _commandFormat;
+
+    public string commandID { get { return _commandId; } }
+    public string commandDesctiption { get { return _commandDescription; } }
+    public string commandFormat {get { return _commandFormat; } }
+
+    public DebugCommandBase(string id, string description,string format)
+    {
+        _commandId = id;
+        _commandDescription = description;
+        _commandFormat = format;
+    }
+}
+
+//Command that does not take in any values
+public class DebugCommand : DebugCommandBase
+{
+    private Action command;
+
+    public DebugCommand(string id, string description, string format, Action command) : base(id, description, format)
+    {
+        this.command = command;
+    }
+
+    public void Invoke()
+    {
+        command.Invoke();
+    }
+
+}
+
+//Command that can take one generic paramater
+public class DebugCommand<T1> : DebugCommandBase
+{
+    private Action<T1> command;
+
+    public DebugCommand(string id, string description, string format, Action<T1> command) : base(id, description, format)
+    {
+        this.command = command;
+    }
+
+    public void Invoke(T1 value)
+    {
+        command.Invoke(value);
+    }
+
+}
---
+++ Basic Unity Debug System/DebugController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BasicDebug
+{
+    public class DebugController : MonoBehaviour
+    {
+        //TODO: Make sure to edit the code to actually take in inputs.
+
+        /* HOW TO MAKE A NEW DEBUG GOMMAND
+        * 
+        * 1. Initialise a public static debug command in the Commands region
+        * 2. On Awake(). Define the debug command in the using the following:
+        * 
+        * [NAME OF COMMAND] = new DebugCommand("ID","Description","Format", () =>
+        * {
+        *      [code of what the command does goes here]
+        * });
+        * 
+        * 3. Add the command variable into the commandList on Awake().
+        */
+
+        //UI variables
+        bool showConsole;
+        bool showHelp;
+        string userInput;
+        Vector2 scroll;
+
+        //All the Command initialisations. 
+        #region Commands;
+
+        public static DebugCommand HELP;
+
+        #endregion
+
+
+        public List<object> commandList;
+
+        //Create the Command List
+        private void Awake()
+        {
+            //Define what the commands do here.
+            #region Command Defitions
+
+            HELP = new DebugCommand("help", "shows a list of commands", "help", () =>
+            {
+                showHelp = true;
+            });
+
+            #endregion
+
+            //Remember to add any new commands to this list
+            commandList = new List<object>
+            {
+                HELP
+            };
+
+        }
+
+        private void onEnter()
+        {
+            if (showConsole)
+            {
+                //Get controller to read and activate input then deletes text
+                HandleInput();
+                userInput = "";
+            }
+        }
+
+
+        //What happens when the debug button is pressed
+        private void onToggle()
+        {
+            showConsole = !showConsole;
+        }
+
+
+
+        //Drawing the Debug UI
+        private void OnGUI()
+        {
+            //Disables debug. If not going to be disabled then move on
+            if (!showConsole) { return; }
+
+
+            float y = 0;
+
+            //Main debug box, where the user can type in their command
+            userInput = GUI.TextField(new Rect(0, y, Screen.width, 20), userInput);
+
+            //Displays the help box. Lising all of the avaliable commands
+            if (showHelp)
+            {
+                GUI.Box(new Rect(0, y + 20, Screen.width, 100), "");
+
+                Rect viewport = new Rect(0, 0, Screen.width - 30, 20 * commandList.Count);
+
+                scroll = GUI.BeginScrollView(new Rect(0, y + 20, Screen.width, 90), scroll, viewport);
+
+                for (int i = 0; i < commandList.Count; i++)
+                {
+                    DebugCommandBase command = commandList[i] as DebugCommandBase;
+
+                    string label = $"{command.commandFormat} - {command.commandDesctiption}";
+
+                    Rect labelRect = new Rect(5, 20 * i, viewport.width - 100, 20);
+
+                    GUI.Label(labelRect, label);
+                }
+
+                GUI.EndScrollView();
+
+            }
+        }
+
+        void HandleInput()
+        {
+
+            string[] properties = userInput.Split(' ');
+
+            //Looks though list of debug commands. If user input equals a commandID, run that debug command.
+            for (int i = 0; i < commandList.Count; i++)
+            {
+                DebugCommandBase commandBase = commandList[i] as DebugCommandBase;
+
+                if (userInput.Contains(commandBase.commandID))
+                {
+                    if (commandList[i] as DebugCommand != null)
+                    {
+                        //Casts to this type and invokes command
+                        (commandList[i] as DebugCommand).Invoke();
+                    }
+                    else if (commandList[i] as DebugCommand<int> != null)
+                    {
+                        (commandList[i] as DebugCommand<int>).Invoke(int.Parse(properties[1]));
+                    }
+                }
+
+            }
+        }
+
+    }
+}
+
